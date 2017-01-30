b3fc1c720dee85e33c5c44d412f17cc544e90b92
Greg Bacus <gbacus28@gmail.com>
2017-01-30 12:20:38 -0600

    Updated changes on instruction menu

--- src/phaser/states/instructionmenu.js
+++ src/phaser/states/instructionmenu.js
@@ -1,17 +1,13 @@
 import Menu from './menu';
 
-var options = [{
+const options = [{
   text: 'Back',
   targetState: 'MainMenu'
 }];
 
-
-// var instructionsText = "/n HERE ARE THE INSTRUCTIONS THAT TAI AND I CREATED!"
-
 export default class MainMenu extends Menu {
   constructor() {
     super(options, 'Instructions');
-    // console.log("HERE IS SUPER.GAME: ", super.game);
   }
 
   create() {
@@ -37,11 +33,3 @@ export default class MainMenu extends Menu {
   }
 
 }
-
-
-
-
-
-// "\n* HOW TO PLAY *\n - Use arrow keys to control the movement of your character.\n - If you are the hunter, chase the prey and collide with them to win the game.\n - If you are the prey, run from the hunter while collecting enough items to gain enough strength to reverse the roles.\n\n * AS WEREWOLF * \n - You start off as the hunter. Kill human before they obtain enough silver to kill you. \n - If human has a sword, run away from human and eat meat to gain enough strength to hunt again! \n - Once all meat has been eaten, kill human! \n Repeat! \n\n * AS HUMAN * \n - Start off by evading the werewolf. \n - Move over the silver pieces to collect them and increase your score. \n - Once you've collected enough silver, you are now the hunter."
-
-// , { fill: '#fff' }
