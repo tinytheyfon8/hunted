411870ae9d7916b73c4dbf4f7238ce858dc8f533
Forrest Miller <forrest.w.miller@icloud.com>
2017-02-03 10:38:04 -0600

    audio sans death yells added and working

--- src/phaser/states/gameover.js
+++ src/phaser/states/gameover.js
@@ -1,7 +1,8 @@
 // Game over state
 
 import einstein from '../assets/images/einstein.png';
-// import gameovermusic from '../assets/audio/FILLMEIN.mp3';
+import gameovermusic from '../assets/audio/gameoverloss.mp3';
+import winmusic from '../assets/audio/anewbegining.mp3';
 
 export default class GameOver extends window.Phaser.State {
 
@@ -9,19 +10,27 @@ export default class GameOver extends window.Phaser.State {
   preload() {
     this.game.load.image('einstein', einstein);
 
-    // this.load.audio('gameovermusic', gameovermusic)
+    this.game.load.audio('gameovermusic', gameovermusic);
+    this.game.load.audio('winmusic', winmusic);
+
   }
 
   // By setting up global variables in the create method, we initialize them when the GameOver state starts
   create() {
     const imageBtn = this.game.add.button(this.game.world.centerX, this.game.world.centerY, 'einstein', this.startGame, this);
-    // const music = this.game.add.audio('gameovermusic')
+    const musicLoss = this.game.add.audio('gameovermusic');
+    const musicWin = this.game.add.audio('winmusic');
+
     // Using the won property of the window object, we display a win message or a lose message.
     const display = window.app.model.won
       ? `YOU WON!  FINAL SCORE: ${window.app.model.score}`
       : 'YOU SUCK';
 
-    // music.play();  
+    // Using the won property we determine which ending music to play;
+    window.app.model.won ? musicWin.play() : musicLoss.play();
+
+
+    // musicLoss.play();  
 
     imageBtn.anchor.set(0.5);
 
